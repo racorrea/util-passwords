5814152ca1134003134b677c56df7069653b4e34
Roddy Andrés Correa T. <racorrea2@utpl.edu.ec>
2018-05-18 19:20:05 -0500

    se carga lógica + escritura de archivo destino

--- src/generadorpassucv/GeneradorPassUCV.java
+++ src/generadorpassucv/GeneradorPassUCV.java
@@ -6,12 +6,16 @@
 package generadorpassucv;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,43 +28,59 @@ public class GeneradorPassUCV {
     /**
      * @param args the command line arguments
      */
-    private static String ARCHIVO_ORIGEN = "C:\\Users\\racorrea2\\txt.txt";
-    private static String ARCHIVO_DESTINO = "C:\\Users\\racorrea2\\txt_2.txt";
+    private static String ARCHIVO_ORIGEN = "D:\\ucv\\txt.txt";
+    private static String ARCHIVO_DESTINO = "D:\\ucv\\txt_2.txt";
 
     public static void main(String[] args) {
         try {
-            leerArchivo(ARCHIVO_ORIGEN);
+            tratarArchivo(ARCHIVO_ORIGEN);
 //            escribirArchivo("C:\\Users\\racorrea2\\txt_2.txt");
         } catch (IOException ex) {
             Logger.getLogger(GeneradorPassUCV.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public static void leerArchivo(String archivo) throws FileNotFoundException, IOException {
+    public static void tratarArchivo(String archivo) throws FileNotFoundException, IOException {
         String cadena;
         String cifrado;
-        char[] cifradoToArray;
+        String cifradoFinal;
+
+        List<Character> cifradoToArray;
         FileReader f = new FileReader(archivo);
         BufferedReader b = new BufferedReader(f);
+        
+        FileWriter fichero = null;
+        PrintWriter pw = null;
+        fichero = new FileWriter(ARCHIVO_DESTINO);
+        pw = new PrintWriter(fichero);
+
+
         while ((cadena = b.readLine()) != null) {
             cifrado = cifrarBase64(cadena);
             cifradoToArray = convertirArreglo(cifrado);
-            logicaPasswords(cifradoToArray);
+            cifradoFinal = logicaPasswords(cifradoToArray);
+            System.out.println(cifradoFinal);
+            pw.println(cifradoFinal);
+            
+            //escribirArchivo(ARCHIVO_DESTINO, cifradoFinal);
         }
         b.close();
-    }
-
-    public static void escribirArchivo(String archivo, String claveEncriptada) throws IOException {
-        FileWriter fichero = null;
-        PrintWriter pw = null;
-        fichero = new FileWriter(archivo);
-        pw = new PrintWriter(fichero);
-        pw.println(claveEncriptada);
         if (null != fichero) {
             fichero.close();
         }
-
     }
+
+//    public static void escribirArchivo(String claveEncriptada) throws IOException {
+//        FileWriter fichero = null;
+//        PrintWriter pw = null;
+//        fichero = new FileWriter(ARCHIVO_DESTINO);
+//        pw = new PrintWriter(fichero);
+//        pw.println(claveEncriptada);
+//        pw.
+//        if (null != fichero) {
+//            fichero.close();
+//        }
+//    }
 
     public static String cifrarBase64(String cadena) {
         byte[] encodedBytes = Base64.getEncoder().encode(cadena.getBytes());
@@ -68,24 +88,39 @@ public class GeneradorPassUCV {
         return cifrado;
     }
 
-    public static char[] convertirArreglo(String cifrado) {
-        char[] arrayCifrado = cifrado.toCharArray();
-        System.out.println(cifrado + " => " + arrayCifrado[0] + " " + arrayCifrado[1] + " " + arrayCifrado[2] + " " + arrayCifrado[3]);
-        return arrayCifrado;
-    }
-    
-    public static void logicaPasswords(char[] arregloPass){
-        char[] arregloResultante = null;
-        String digitosPosTres = "?43";
-        for(int i=0; i<arregloPass.length; i++){
-            //arregloResultante[i] = arregloPass[i];
-//            if(i == 2){ //posicion 3
-//                arregloResultante[i] = digitosPosTres.charAt(0);
-//            }
-            System.out.println(" ---> " + arregloPass[i]);
+    public static List<Character> convertirArreglo(String cifrado) {
+        List<Character> listaCaracteresCifrado = new ArrayList<Character>();
+        for (char c : cifrado.toCharArray()) {
+            listaCaracteresCifrado.add(c);
         }
-        System.out.println("---------***------------");
-        
+        return listaCaracteresCifrado;
+    }
+
+    public static String logicaPasswords(List<Character> listaPass) {
+        String digitosPosTres = "?43";
+        String digitosPosOnce = "16?c";
+
+        listaPass.add(3, digitosPosTres.charAt(0));
+        listaPass.add(4, digitosPosTres.charAt(1));
+        listaPass.add(5, digitosPosTres.charAt(2));
+
+        listaPass.add(13, digitosPosOnce.charAt(0));
+        listaPass.add(14, digitosPosOnce.charAt(1));
+        listaPass.add(15, digitosPosOnce.charAt(2));
+        listaPass.add(16, digitosPosOnce.charAt(3));
+
+        String cifradoFinal = arrayListToString(listaPass);
+        return cifradoFinal;
+    }
+
+    public static String arrayListToString(List<Character> lista) {
+        StringBuilder sb = new StringBuilder();
+        for (Character ch : lista) {
+            sb.append(ch);
+        }
+
+        String cifrado = sb.toString();
+        return cifrado;
     }
 
 }
